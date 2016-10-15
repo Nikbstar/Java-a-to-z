e006f752128fa62e7ad868409133d720cb44adf2
Nikolay <Nikbstar@gmail.com>
2016-10-15 23:31:59 +0500

    Add test to Calculate

--- chapter_001/src/main/java/ru/nkotkin/Calculate.java
+++ chapter_001/src/main/java/ru/nkotkin/Calculate.java
@@ -2,10 +2,15 @@ package ru.nkotkin;
 
 public class Calculate {
 	public static void main(String[] args) {
+		Calculate calc = new Calculate();
+		System.out.println(calc.echo("ahh"));
 		System.out.println("Calculate...");
 		int first = Integer.valueOf(args[0]);
 		int second = Integer.valueOf(args[1]);
 		int summ = first + second;
 		System.out.println("Sum " + summ);
+	}
+	public String echo(String value) {
+		return String.format("%s %s %s", value, value, value);
 	}
 }
