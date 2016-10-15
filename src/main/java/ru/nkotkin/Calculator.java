4bd0c6b1b2990c682f80a857c8a5d64c81571fbf
Nikolay <Nikbstar@gmail.com>
2016-10-15 21:39:00 +0500

    add ArgRunner and modify Calculaor (don't work ConsoleIO)

--- src/main/java/ru/nkotkin/Calculator.java
+++ src/main/java/ru/nkotkin/Calculator.java
@@ -1,16 +1,57 @@
 package ru.nkotkin;
 
 public class Calculator {
-	private int result;
-	public void add(int ... params) {
-		for(Integer param : params) {
+	private double result;
+	// Слодение
+	public void add(double ... params) {
+		for(Double param : params) {
 			this.result += param;
 		}
 	}
-	public int getResult() {
+	// Вычитание
+	public void substract(double first, double second) {
+		this.result = first - second;
+	}
+	// Умножение
+	public void multiple(double first, double second) {
+		this.result = first * second;
+	}
+	// Деление
+	public void div(double first, double second) {
+		if(second != 0d) {
+			this.result = first / second;
+		} else {
+			throw new ArithmeticException("Cound not div by 0");
+		}
+	}
+	// Возведение в степень
+	public void expand(double first, int second) {
+		double res = 1;
+		for(int index = 0; index < second; index++) {
+			res *= first;
+		}
+		this.result = res;
+	}
+	// Вычисляем арифметическую операцию на основании входных значений
+	public void calc(String operation, double first, double second) {
+		if("+".equals(operation)) {
+			this.add(first, second);
+		} else if("-".equals(operation)) {
+			this.substract(first, second);
+		} else if("*".equals(operation)) {
+			this.multiple(first, second);
+		} else if("/".equals(operation)) {
+			this.div(first, second);
+		} else if("e".equals(operation)) {
+			this.expand(first, (int) second);
+		} else {
+			throw new UnsupportedOperationException();
+		}
+	}
+	public double getResult() {
 		return result;
 	}
-	public void clearResult() {
+/*	public void clearResult() {
 		this.result = 0;
-	}
+	}*/
 }
---
+++ src/main/java/ru/nkotkin/ConsoleIO.java
@@ -0,0 +1,19 @@
+package ru.nkotkin;
+public class ConsoleIO implements IO {
+	private static final Logger Log = LoggerFactory.getLogger(ConsoleIO.class);
+	private final Scanner scanner;
+	private final PrintStream out;
+	
+	public ConsoleIO(final Scanner scanner, final PrintStream out) {
+		this.scanner = scanner;
+		this.out = out;
+	}
+	@Override
+	public String read() {
+		return this.scanner.next();
+	}
+	@Override
+	public void println(Object value) {
+		this.out.println(value);
+	}
+}
