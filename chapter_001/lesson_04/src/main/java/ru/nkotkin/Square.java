0344b5f51442b4b168b9de9725980076a0e01629
Nikolay <Nikbstar@gmail.com>
2016-10-25 18:23:32 +0500

    realized method Square.calculate

--- chapter_001/lesson_04/src/main/java/ru/nkotkin/Square.java
+++ chapter_001/lesson_04/src/main/java/ru/nkotkin/Square.java
@@ -1,5 +1,26 @@
 package ru.nkotkin;
 
+/**
+ * Создать класс Square в нем один методы float calculate(int x)
+ * и void show(start, finish, step). Метод должен вычислять значение
+ * функции y = a*x2 + b * x + c; параметры a b c задаются через конструктор.
+ * void show(start, finish, step) - вывести на экран значение функции в
+ * диапазоне от x1 до x2 c шагом step. 
+ *
+ * 2. Обязательно нужно добавить тесты проверяющие методы.
+ */
 public class Square {
+	private float a, b, c;
 	
+	public Square(float a, float b, float c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	public float calculate (int x) {
+		return this.a * (x * x) + this.b * x + this.c;
+	}
+	public void show(float start, float finish, float step) {
+		
+	}
 }
--- chapter_001/lesson_04/src/test/java/ru/nkotkin/SquareTest.java
+++ chapter_001/lesson_04/src/test/java/ru/nkotkin/SquareTest.java
@@ -1,9 +1,15 @@
 package ru.nkotkin;
+
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class SquareTest {
-	//@Test
-	public void whenThen() throws Exception {
+	@Test
+	public void whenAddXIntoCalculateThenReturnY() throws Exception {
+		Square square = new Square(1f, 1f, 1f);
+		float result = square.calculate(1);
+		assertThat(result, is(3f));
 		
 	}
 }
