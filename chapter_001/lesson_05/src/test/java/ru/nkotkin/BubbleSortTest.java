e33ff68b3d810dea77707bc791b59078c1540ff7
Nikolay <Nikbstar@gmail.com>
2016-10-30 22:07:57 +0500

    add BubbleSort

---
+++ chapter_001/lesson_05/src/test/java/ru/nkotkin/BubbleSortTest.java
@@ -0,0 +1,12 @@
+package ru.nkotkin;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class BubbleSortTest {
+	@Test
+	public void whenThen() {
+		
+	}
+}
