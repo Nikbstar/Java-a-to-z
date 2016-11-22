4023c4d05fc44a7b94d9033626e450d00412944f
Nikbstar <Nikbstar@gmail.com>
2016-11-22 16:02:59 +0500

    add Tracker

---
+++ chapter_002/tracker/src/main/java/ru/nkotkin/models/Bug.java
@@ -0,0 +1,7 @@
+package ru.nkotkin.models;
+
+/**
+ * Created by Nikolay Kotkin on 21.11.2016.
+ */
+public class Bug extends Item {
+}
---
+++ chapter_002/tracker/src/main/java/ru/nkotkin/models/Item.java
@@ -0,0 +1,62 @@
+package ru.nkotkin.models;
+
+/**
+ * Created by Nikolay Kotkin on 21.11.2016.
+ */
+public class Item {
+    /**
+     * Item name.
+     */
+    private String name;
+    /**
+     * Item description.
+     */
+    private String description;
+    /**
+     * Create date.
+     */
+    private long create;
+
+    /**
+     * Default constructor.
+     */
+    public Item() {
+    }
+
+    /**
+     * Constructor.
+     * @param nameArg        - set name
+     * @param descriptionArg - set description
+     * @param createArg      - set create time
+     */
+    public Item(final String nameArg, final String descriptionArg, final long createArg) {
+        this.name = nameArg;
+        this.description = descriptionArg;
+        this.create = createArg;
+    }
+
+    /**
+     * getter for name.
+     * @return string name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * getter for description.
+     * @return string description
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * getter for create.
+     * @return long date
+     */
+    public long getCreate() {
+        return this.create;
+    }
+
+}
