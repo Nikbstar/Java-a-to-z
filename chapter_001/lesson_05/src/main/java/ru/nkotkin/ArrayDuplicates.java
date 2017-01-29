5e0e467fa4d61870b6e7fe0b29ee05113bc30778
Nikolay Kotkin <nikbstar@gmail.com>
2017-01-30 00:08:08 +0500

    Update Duplicates

--- chapter_001/lesson_05/src/main/java/ru/nkotkin/ArrayDuplicates.java
+++ chapter_001/lesson_05/src/main/java/ru/nkotkin/ArrayDuplicates.java
@@ -1,5 +1,7 @@
 package ru.nkotkin;
 
+import java.util.Arrays;
+
 /**
  * Дано. Массив строк. String[] - нужно убрать дубликаты.
  *
@@ -12,56 +14,76 @@ package ru.nkotkin;
 public class ArrayDuplicates {
 
     /**
-     * ReRemove duplicates.
-     * @param values - Array of strings
+     * Removing duplicates.
+     * @author P. Arseniev.
+     * @param array array of strings.
+     * @return array without duplicates.
      */
-    public final void doRemove(final String[] values) {
-        for (int outsideIterator = 0;
-             outsideIterator != values.length;
-             outsideIterator++) {
-            if (values[outsideIterator] == null) {
-                continue;
-            }
-            for (int insideIterator = values.length - 1;
-                 insideIterator != outsideIterator;
-                 insideIterator--) {
-                if (this.equals(values[insideIterator], values[outsideIterator])) {
-                    this.moveNull(values, insideIterator);
+    public final String[] removeDuplicates(String[] array) {
+        int limit = array.length;
+        for (int out = 0; out != limit; out++) {
+            for (int in = out + 1; in != limit; in++) {
+                if (array[out].equals(array[in])) {
+                    array[out] = array[limit - 1];
+                    limit--;
+                    in--;
                 }
             }
         }
+        return Arrays.copyOf(array, limit);
     }
-
-    /**
-     * find equals.
-     * @param str1 - string
-     * @param str2 - string
-     * @return true if strings is equils
-     */
-    private boolean equals(final String str1, final String str2) {
-        /**
-         * result
-         */
-        boolean result;
-
-        if (str1 == null) {
-            result = str2 == null;
-        } else {
-            result = str1.equals(str2);
-        }
-        return result;
-    }
-
-    /**
-     * move nulls back.
-     * @param values - array of strings
-     * @param iterator - position of null
-     */
-    private void moveNull(final String[] values, final int iterator) {
-        for (int i = iterator; i != values.length - 1; i++) {
-            values[iterator] = values[iterator + 1];
-        }
-        values[values.length - 1] = null;
-    }
+//    /**
+//     * ReRemove duplicates.
+//     * @author N. Kotkin.
+//     * @param values - Array of strings
+//     */
+//    public final void doRemove(final String[] values) {
+//        for (int outsideIterator = 0;
+//             outsideIterator != values.length;
+//             outsideIterator++) {
+//            if (values[outsideIterator] == null) {
+//                continue;
+//            }
+//            for (int insideIterator = values.length - 1;
+//                 insideIterator != outsideIterator;
+//                 insideIterator--) {
+//                if (this.equals(values[insideIterator], values[outsideIterator])) {
+//                    this.moveNull(values, insideIterator);
+//                }
+//            }
+//        }
+//    }
+//
+//    /**
+//     * find equals.
+//     * @param str1 - string
+//     * @param str2 - string
+//     * @return true if strings is equils
+//     */
+//    private boolean equals(final String str1, final String str2) {
+//        /**
+//         * result
+//         */
+//        boolean result;
+//
+//        if (str1 == null) {
+//            result = str2 == null;
+//        } else {
+//            result = str1.equals(str2);
+//        }
+//        return result;
+//    }
+//
+//    /**
+//     * move nulls back.
+//     * @param values - array of strings
+//     * @param iterator - position of null
+//     */
+//    private void moveNull(final String[] values, final int iterator) {
+//        for (int i = iterator; i != values.length - 1; i++) {
+//            values[iterator] = values[iterator + 1];
+//        }
+//        values[values.length - 1] = null;
+//    }
 
 }
--- chapter_001/lesson_05/src/test/java/ru/nkotkin/ArrayDuplicatesTest.java
+++ chapter_001/lesson_05/src/test/java/ru/nkotkin/ArrayDuplicatesTest.java
@@ -1,28 +1,47 @@
 package ru.nkotkin;
 
 import org.junit.Test;
+
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.core.Is.is;
 
 /**
  * Tests for ArrayDuplicates.java.
  */
 public class ArrayDuplicatesTest {
 
-    /**
-     * whenAddArrayThenDeleteDuplicatesFromArray.
-     * @throws Exception - any
-     */
     @Test
-    public final void whenAddArrayThenDeleteDuplicatesFromArray() throws Exception {
-
-        ArrayDuplicates dblRemove = new ArrayDuplicates();
-
-        String[] values = {"one", "one", "one", "two"};
-        String[] result = {"one", "two", null, null};
-
-        dblRemove.doRemove(values);
-        assertThat(result, is(values));
+    public void whenArrayHasDuplicatesThenDeleteIt() throws Exception {
+        ArrayDuplicates duplicates = new ArrayDuplicates();
+        String[] input = {"Lisa", "Olga", "Lisa", "Sveta", "Valeria"};
+        String[] except = {"Lisa", "Olga", "Sveta", "Valeria"};
+        String[] result = duplicates.removeDuplicates(input);
+        assertThat(result, arrayContainingInAnyOrder(except));
     }
+
+    @Test
+    public void whenArrayHasAllDuplicatesThenDeleteIt() throws Exception {
+        ArrayDuplicates duplicates = new ArrayDuplicates();
+        String[] input = {"1", "1", "1", "1", "1"};
+        String[] except = {"1"};
+        String[] result = duplicates.removeDuplicates(input);
+        assertThat(result, arrayContainingInAnyOrder(except));
+    }
+
+//    /**
+//     * whenAddArrayThenDeleteDuplicatesFromArray.
+//     * @throws Exception - any
+//     */
+//    @Test
+//    public final void whenAddArrayThenDeleteDuplicatesFromArray() throws Exception {
+//
+//        ArrayDuplicates dblRemove = new ArrayDuplicates();
+//
+//        String[] values = {"one", "one", "one", "two"};
+//        String[] result = {"one", "two", null, null};
+//
+//        dblRemove.doRemove(values);
+//        assertThat(result, is(values));
+//    }
 
 }
